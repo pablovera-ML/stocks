2dc4995de475b76ba90b7e4e0c55277ab6f9631a
Pablo Vera <pablovera@Pablos-MacBook-Pro.local>
2023-01-12 02:01:15 -0300

    changing data downloader

--- main.py
+++ main.py
@@ -21,45 +21,44 @@ engine = utils.get_engine()
 
 def pull_data_from_yfinance():
     for interval in params.intervals:
-        print(f"Downloading data for interval {interval}")
-        data = yfi.download(  # or pdr.get_data_yahoo(...
-            # tickers list or string as well
-            tickers=tickers_list,
-
-            # use "period" instead of start/end
-            # valid periods: 1d,5d,1mo,3mo,6mo,1y,2y,5y,10y,ytd,max
-            # (optional, default is '1mo')
-            period="max",
-
-            # fetch data by interval (including intraday if period < 60 days)
-            # valid intervals: 1m,2m,5m,15m,30m,60m,90m,1h,1d,5d,1wk,1mo,3mo
-            # (optional, default is '1d')
-            interval=interval,
-
-            # group by ticker (to access via data['SPY'])
-            # (optional, default is 'column')
-            group_by='ticker',
-
-            # adjust all OHLC automatically
-            # (optional, default is False)
-            auto_adjust=False,
-
-            # download pre/post regular market hours data
-            # (optional, default is False)
-            prepost=False,
-
-            # use threads for mass downloading? (True/False/Integer)
-            # (optional, default is True)
-            threads=True,
-
-            # proxy URL scheme use use when downloading?
-            # (optional, default is None)
-            proxy=None
-        )
-
         for ticker_name in tickers_list:
+            print(f"Downloading data for {ticker_name} and interval {interval}")
+            data = yfi.download(  # or pdr.get_data_yahoo(...
+                # tickers list or string as well
+                tickers=ticker_name,
+
+                # use "period" instead of start/end
+                # valid periods: 1d,5d,1mo,3mo,6mo,1y,2y,5y,10y,ytd,max
+                # (optional, default is '1mo')
+                period="max",
+
+                # fetch data by interval (including intraday if period < 60 days)
+                # valid intervals: 1m,2m,5m,15m,30m,60m,90m,1h,1d,5d,1wk,1mo,3mo
+                # (optional, default is '1d')
+                interval=interval,
+
+                # group by ticker (to access via data['SPY'])
+                # (optional, default is 'column')
+                group_by='ticker',
+
+                # adjust all OHLC automatically
+                # (optional, default is False)
+                auto_adjust=False,
+
+                # download pre/post regular market hours data
+                # (optional, default is False)
+                prepost=False,
+
+                # use threads for mass downloading? (True/False/Integer)
+                # (optional, default is True)
+                threads=True,
+
+                # proxy URL scheme use use when downloading?
+                # (optional, default is None)
+                proxy=None
+            )
             print("Creating table in DB")
-            ticker_ohlc = data[ticker_name]
+            ticker_ohlc = data
             ticker_ohlc.columns = list(map(lambda x: x.lower(), ticker_ohlc.columns.to_list()))
             table_name = f"{params.tickers[ticker_name]}_{interval}"
             ticker_ohlc.dropna().to_sql(table_name,
